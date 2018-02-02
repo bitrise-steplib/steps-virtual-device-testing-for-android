1678cb6dc78c6d0a39aa20f5d5d69d7e5328a35b
trapacska <trapacska@gmail.com>
2018-02-02 13:15:10 +0100

    added warning of empty APIBaseURL, updated step input description

--- main.go
+++ main.go
@@ -163,8 +163,10 @@ func (configs ConfigsModel) print() {
 }
 
 func (configs ConfigsModel) validate() error {
-
 	if err := input.ValidateIfNotEmpty(configs.APIBaseURL); err != nil {
+		if os.Getenv("BITRISE_IO") != "" {
+			log.Warnf("Warning: please make sure that Virtual Device Testing add-on is turned on under your app's settings tab.")
+		}
 		return fmt.Errorf("Issue with APIBaseURL: %s", err)
 	}
 	if err := input.ValidateIfNotEmpty(configs.APIToken); err != nil {
