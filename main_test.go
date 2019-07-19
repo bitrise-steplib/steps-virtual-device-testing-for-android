fe59f245316a8b9b6ac05004326b5c78b1e40437
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-07-19 14:32:23 +0200

    proper table driven tests

--- main_test.go
+++ main_test.go
@@ -5,27 +5,56 @@ import (
 )
 
 func TestValidateTimeout(t *testing.T) {
-	if err := validateTimeout("2.123456789"); err != nil {
-		t.Errorf("error: %s", err)
+	testcases := []struct{
+		title    string
+		duration string
+		wantE    bool
+	}{
+		{
+			title:    "nine digit fraction",
+			duration: "2.123456789",
+			wantE:    false,
+		},
+		{
+			title:    "three digit fraction",
+			duration: "2.123",
+			wantE:    false,
+		},
+		{
+			title:    "plain integer",
+			duration: "2",
+			wantE:    false,
+		},
+		{
+			title:    "empty value",
+			duration: "",
+			wantE:    true,
+		},
+		{
+			title:    "invalid fraction delimiter",
+			duration: "2,123",
+			wantE:    true,
+		},
+		{
+			title:    "missing fraction digits",
+			duration: "2.",
+			wantE:    true,
+		},
+		{
+			title:    "missing integer digits",
+			duration: ".123",
+			wantE:    true,
+		},
+		{
+			title:    "only a dot",
+			duration: ".",
+			wantE:    true,
+		},
 	}
-	if validateTimeout("2.123") != nil {
-		t.Errorf("error")
-	}
-	if validateTimeout("2") != nil {
-		t.Errorf("error")
-	}
+	for _, tt := range testcases {
+		if err := validateTimeout(tt.duration); (err != nil) != tt.wantE {
+			t.Errorf("%s: got error (%s), but want error is %t", tt.title, err, tt.wantE)
+		}
 
-	if validateTimeout("") == nil {
-		t.Errorf("error")
-	}
-	if validateTimeout("2.") == nil {
-		t.Errorf("error")
-	}
-	if validateTimeout(".1") == nil {
-		t.Errorf("error")
-	}
-	if validateTimeout(".") == nil {
-		t.Errorf("error")
-	}
-
+	} 
 }
