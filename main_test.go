d65a39b24e409252d1f631d96511acc4a976270b
Laszlo Szucs <laszlo.szucs@bitrise.io>
2019-08-05 16:10:15 +0200

    refactor: delete test for deleted function

--- main_test.go
+++ main_test.go
@@ -4,61 +4,6 @@ import (
 	"testing"
 )
 
-func TestValidateTimeout(t *testing.T) {
-	testcases := []struct{
-		title    string
-		duration string
-		wantE    bool
-	}{
-		{
-			title:    "nine digit fraction",
-			duration: "2.123456789",
-			wantE:    false,
-		},
-		{
-			title:    "three digit fraction",
-			duration: "2.123",
-			wantE:    false,
-		},
-		{
-			title:    "plain integer",
-			duration: "2",
-			wantE:    false,
-		},
-		{
-			title:    "empty value",
-			duration: "",
-			wantE:    true,
-		},
-		{
-			title:    "invalid fraction delimiter",
-			duration: "2,123",
-			wantE:    true,
-		},
-		{
-			title:    "missing fraction digits",
-			duration: "2.",
-			wantE:    true,
-		},
-		{
-			title:    "missing integer digits",
-			duration: ".123",
-			wantE:    true,
-		},
-		{
-			title:    "only a dot",
-			duration: ".",
-			wantE:    true,
-		},
-	}
-	for _, tt := range testcases {
-		if err := validateTimeout(tt.duration); (err != nil) != tt.wantE {
-			t.Errorf("%s: got error (%s), but want error is %t", tt.title, err, tt.wantE)
-		}
-
-	} 
-}
-
 func TestNormalize(t *testing.T) {
     cases := []struct{
         title string
