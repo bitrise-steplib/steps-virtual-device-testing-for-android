0cf8e1150a6e6d862c9891ee4774dd7624669420
Laszlo Pusok <laszlo.pusok@bitrise.io>
2019-08-30 18:08:17 +0200

    app_path is now required. Moving functions to seperate files.

---
+++ config.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/bitrise-io/go-utils/log"
+	testing "google.golang.org/api/testing/v1"
+)
+
+// ConfigsModel ...
+type ConfigsModel struct {
+	// api
+	APIBaseURL string `env:"api_base_url"`
+	BuildSlug  string `env:"BITRISE_BUILD_SLUG,required"`
+	AppSlug    string `env:"BITRISE_APP_SLUG,required"`
+	APIToken   string `env:"api_token,required"`
+
+	// shared
+	AppPath         string `env:"app_path"`
+	TestApkPath     string `env:"test_apk_path"`
+	TestType        string `env:"test_type,opt[instrumentation,robo,gameloop]"`
+	TestDevicesList string `env:"test_devices"`
+	TestDevices     []*testing.AndroidDevice
+	AppPackageID    string `env:"app_package_id"`
+
+	// test setup
+	AutoGoogleLogin          bool   `env:"auto_google_login,opt[true,false]"`
+	EnvironmentVariablesList string `env:"environment_variables"`
+	EnvironmentVariables     []*testing.EnvironmentVariable
+	ObbFilesList             string `env:"obb_files_list"`
+	ObbFiles                 []string
+
+	// shared debug
+	TestTimeout           float64 `env:"test_timeout,required"`
+	FlakyTestAttempts     int     `env:"num_flaky_test_attempts,range[0..10]"`
+	DownloadTestResults   bool    `env:"download_test_results,opt[true,false]"`
+	DirectoriesToPullList string  `env:"directories_to_pull"`
+	DirectoriesToPull     []string
+	VerboseLog            bool `env:"use_verbose_log,opt[true,false]"`
+
+	// instrumentation
+	InstTestPackageID   string `env:"inst_test_package_id"`
+	InstTestRunnerClass string `env:"inst_test_runner_class"`
+	InstTestTargets     string `env:"inst_test_targets"`
+	UseOrchestrator     bool   `env:"inst_use_orchestrator,opt[true,false]"`
+
+	// robo
+	RoboInitialActivity string `env:"robo_initial_activity"`
+	RoboDirectives      string `env:"robo_directives"`
+	RoboScenarioFile    string `env:"robo_scenario_file"`
+	RoboMaxDepth        string `env:"robo_max_depth"`
+	RoboMaxSteps        string `env:"robo_max_steps"`
+
+	// loop
+	LoopScenarios       string `env:"loop_scenarios"`
+	LoopScenarioLabels  string `env:"loop_scenario_labels"`
+	LoopScenarioNumbers string `env:"loop_scenario_numbers"`
+
+	// deprecated
+	ApkPath string `env:"apk_path"`
+}
+
+func (configs *ConfigsModel) print() {
+	log.Infof("Configs:")
+	log.Printf("- AppPath: %s", configs.AppPath)
+	if configs.ApkPath != "" {
+		log.Printf("- ApkPath: %s", configs.ApkPath)
+	}
+	if configs.AppPackageID != "" {
+		log.Printf("- AppPackageID: %s", configs.AppPackageID)
+	}
+	log.Printf("- TestTimeout: %f", configs.TestTimeout)
+	log.Printf("- FlakyTestAttempts: %d", configs.FlakyTestAttempts)
+	log.Printf("- DownloadTestResults: %t", configs.DownloadTestResults)
+	log.Printf("- DirectoriesToPull: %s", configs.DirectoriesToPullList)
+	log.Printf("- AutoGoogleLogin: %t", configs.AutoGoogleLogin)
+	log.Printf("- EnvironmentVariables: %s", configs.EnvironmentVariablesList)
+	log.Printf("- ObbFilesList: %s", configs.ObbFilesList)
+	log.Printf("- TestDevices:\n---")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 3, ' ', 0)
+	if _, err := fmt.Fprintln(w, "Model\tAPI Level\tLocale\tOrientation\t"); err != nil {
+		failf("Failed to write in tabwriter, error: %s", err)
+	}
+	scanner := bufio.NewScanner(strings.NewReader(configs.TestDevicesList))
+	for scanner.Scan() {
+		device := scanner.Text()
+		device = strings.TrimSpace(device)
+		if device == "" {
+			continue
+		}
+
+		deviceParams := strings.Split(device, ",")
+
+		if len(deviceParams) != 4 {
+			continue
+		}
+
+		if _, err := fmt.Fprintln(w, fmt.Sprintf("%s\t%s\t%s\t%s\t", deviceParams[0], deviceParams[1], deviceParams[3], deviceParams[2])); err != nil {
+			failf("Failed to write in tabwriter, error: %s", err)
+		}
+	}
+	if err := w.Flush(); err != nil {
+		log.Errorf("Failed to flush writer, error: %s", err)
+	}
+
+	log.Printf("---")
+	log.Printf("- TestType: %s", configs.TestType)
+	// instruments
+	if configs.TestType == testTypeInstrumentation {
+		log.Printf("- TestApkPath: %s", configs.TestApkPath)
+		log.Printf("- InstTestPackageID: %s", configs.InstTestPackageID)
+		log.Printf("- InstTestRunnerClass: %s", configs.InstTestRunnerClass)
+		log.Printf("- InstTestTargets: %s", configs.InstTestTargets)
+		log.Printf("- UseOrchestrator: %t", configs.UseOrchestrator)
+	}
+
+	//robo
+	if configs.TestType == testTypeRobo {
+		log.Printf("- RoboInitialActivity: %s", configs.RoboInitialActivity)
+		log.Printf("- RoboScenarioFile: %s", configs.RoboScenarioFile)
+		log.Printf("- RoboDirectives: %s", configs.RoboDirectives)
+		log.Printf("- RoboMaxDepth: %s", configs.RoboMaxDepth)
+		log.Printf("- RoboMaxSteps: %s", configs.RoboMaxSteps)
+	}
+
+	// loop
+	if configs.TestType == "gameloop" {
+		log.Printf("- LoopScenarios: %s", configs.LoopScenarios)
+		log.Printf("- LoopScenarioLabels: %s", configs.LoopScenarioLabels)
+		log.Printf("- LoopScenarioNumbers: %s", configs.LoopScenarioNumbers)
+	}
+}
+
+func (configs *ConfigsModel) validate() error {
+	configs.migrate()
+
+	if strings.TrimSpace(configs.APIBaseURL) == "" {
+		if _, set := os.LookupEnv("BITRISE_IO"); !set {
+			log.Warnf("Warning: please make sure that Virtual Device Testing add-on is turned on under your app's settings tab.")
+		}
+		return fmt.Errorf("- APIBaseURL: required variable is not present")
+	}
+
+	if strings.TrimSpace(configs.AppPath) == "" {
+		return fmt.Errorf("- AppPath: required variable is not present")
+	}
+	if _, err := os.Stat(configs.AppPath); err != nil {
+		return fmt.Errorf("- AppPath: failed to get file info, error: %s", err)
+	}
+
+	if configs.TestType == testTypeInstrumentation {
+		if strings.TrimSpace(configs.TestApkPath) == "" {
+			return fmt.Errorf("- TestApkPath: required variable is not present. Is it possible that you used gradle-runner step and forgot to set `assembleDebugAndroidTest` task?")
+		}
+		if _, err := os.Stat(configs.TestApkPath); err != nil {
+			return fmt.Errorf("- TestApkPath: failed to get file info, error: %s. Is it possible that you used gradle-runner step and forgot to set `assembleDebugAndroidTest` task?", err)
+		}
+	}
+
+	configs.RoboScenarioFile = strings.TrimSpace(configs.RoboScenarioFile)
+	if configs.TestType == testTypeRobo && configs.RoboScenarioFile != "" {
+		if _, err := os.Stat(configs.RoboScenarioFile); err != nil {
+			return fmt.Errorf("- RoboScenarioFile: failed to get file info, error: %s", err)
+		}
+	}
+
+	if configs.TestTimeout > float64(maxTimeoutSeconds) {
+		log.Warnf("timeout value (%f) is greater than available maximum (%f). Maximum will be used instead.", configs.TestTimeout, maxTimeoutSeconds)
+		configs.TestTimeout = maxTimeoutSeconds
+	}
+
+	var err error
+	if configs.TestDevices, err = parseDeviceList(configs.TestDevicesList); err != nil {
+		return fmt.Errorf("- TestDevices: %s", err)
+	}
+
+	if configs.ObbFiles, err = parseObbFilesList(configs.ObbFilesList); err != nil {
+		return fmt.Errorf("- ObbFiles: %s", err)
+	}
+
+	configs.DirectoriesToPull = parseDirectoriesToPull(configs.DirectoriesToPullList)
+	configs.EnvironmentVariables = parseTestSetupEnvVars(configs.EnvironmentVariablesList)
+
+	return nil
+}
+
+func (configs *ConfigsModel) migrate() {
+	if configs.ApkPath != "" {
+		log.Warnf("'Apk path' (apk_path) input is deprected, use 'App path' (app_path) instead.")
+		log.Warnf("'Apk path' (%s) is specified, overrides App path (%s)", configs.ApkPath, configs.AppPath)
+		configs.AppPath = configs.ApkPath
+	}
+	if configs.AppPackageID != "" {
+		log.Warnf("'App package ID' (app_package_id) input is deprecated. Leave empty to automatically extract it from the App manifest")
+	}
+	if configs.InstTestPackageID != "" {
+		log.Warnf("'Test package ID' (inst_test_package_id) input is deprecatad. Leave empty to automatically extract it from the App manifest")
+	}
+}
+
+func parseDeviceList(deviceList string) ([]*testing.AndroidDevice, error) {
+	var testDevices []*testing.AndroidDevice
+
+	scanner := bufio.NewScanner(strings.NewReader(deviceList))
+	for scanner.Scan() {
+		device := scanner.Text()
+		device = strings.TrimSpace(device)
+		if device == "" {
+			continue
+		}
+
+		deviceParams := strings.Split(device, ",")
+		if len(deviceParams) != 4 {
+			return nil, fmt.Errorf("invalid test device configuration: %s", device)
+		}
+
+		testDevices = append(testDevices, &testing.AndroidDevice{
+			AndroidModelId:   deviceParams[0],
+			AndroidVersionId: deviceParams[1],
+			Locale:           deviceParams[2],
+			Orientation:      deviceParams[3],
+		})
+	}
+
+	return testDevices, nil
+}
+
+func parseObbFilesList(obbFilesList string) ([]string, error) {
+	var obbFiles []string
+	files := strings.Split(obbFilesList, "\n")
+
+	for _, file := range files {
+		file = strings.TrimSpace(file)
+		if file == "" {
+			continue
+		}
+		if _, err := os.Stat(file); err != nil {
+			return nil, fmt.Errorf("could not get file info for obb file (%s), error: %s", file, err)
+		}
+
+		obbFiles = append(obbFiles, file)
+	}
+
+	return obbFiles, nil
+}
+
+func parseDirectoriesToPull(directoriesToPullList string) []string {
+	scanner := bufio.NewScanner(strings.NewReader(directoriesToPullList))
+	directoriesToPull := []string{}
+	for scanner.Scan() {
+		path := scanner.Text()
+		path = strings.TrimSpace(path)
+		if path == "" {
+			continue
+		}
+		directoriesToPull = append(directoriesToPull, path)
+	}
+
+	return directoriesToPull
+}
+
+func parseTestSetupEnvVars(envVarList string) []*testing.EnvironmentVariable {
+	scanner := bufio.NewScanner(strings.NewReader(envVarList))
+	envs := []*testing.EnvironmentVariable{}
+	for scanner.Scan() {
+		envStr := scanner.Text()
+
+		if envStr == "" {
+			continue
+		}
+
+		if !strings.Contains(envStr, "=") {
+			continue
+		}
+
+		envStrSplit := strings.Split(envStr, "=")
+		envKey := envStrSplit[0]
+		envValue := strings.Join(envStrSplit[1:], "=")
+
+		envs = append(envs, &testing.EnvironmentVariable{Key: envKey, Value: envValue})
+	}
+
+	return envs
+}
---
+++ test_api.go
@@ -0,0 +1,321 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/bitrise-io/go-utils/log"
+	testing "google.golang.org/api/testing/v1"
+)
+
+// TestAsset describes a requested test asset
+type TestAsset struct {
+	UploadURL string `json:"uploadUrl"`
+	GcsPath   string `json:"gcsPath"`
+	Filename  string `json:"filename"`
+}
+
+// TestAssetsAndroid describes requested Android test asset and as the returned test asset upload URLs
+type TestAssetsAndroid struct {
+	isBundle   bool
+	testApp    *TestAsset
+	Apk        TestAsset   `json:"apk,omitempty"`
+	Aab        TestAsset   `json:"aab,omitmepty"`
+	TestApk    TestAsset   `json:"testApk,omitempty"`
+	RoboScript TestAsset   `json:"roboScript,omitempty"`
+	ObbFiles   []TestAsset `json:"obbFiles,omitempty"`
+}
+
+func uploadTestAssets(configs ConfigsModel) (TestAssetsAndroid, error) {
+	var testAssets TestAssetsAndroid
+
+	url := configs.APIBaseURL + "/assets/android/" + configs.AppSlug + "/" + configs.BuildSlug + "/" + configs.APIToken
+
+	if strings.ToLower(filepath.Ext(configs.AppPath)) == ".aab" {
+		testAssets.isBundle = true
+	}
+	log.Debugf("App path (%s), is bundle: %t", configs.AppPath, testAssets.isBundle)
+
+	var requestedAssets TestAssetsAndroid
+	if testAssets.isBundle {
+		requestedAssets.Aab = TestAsset{
+			Filename: filepath.Base(configs.AppPath),
+		}
+	} else {
+		requestedAssets.Apk = TestAsset{
+			Filename: filepath.Base(configs.AppPath),
+		}
+	}
+	if configs.TestType == testTypeInstrumentation {
+		requestedAssets.TestApk = TestAsset{
+			Filename: filepath.Base(configs.TestApkPath),
+		}
+	}
+	if configs.TestType == testTypeRobo && configs.RoboScenarioFile != "" {
+		requestedAssets.RoboScript = TestAsset{
+			Filename: filepath.Base(configs.RoboScenarioFile),
+		}
+	}
+	for _, obbFile := range configs.ObbFiles {
+		requestedAssets.ObbFiles = append(requestedAssets.ObbFiles, TestAsset{
+			Filename: filepath.Base(obbFile),
+		})
+	}
+
+	log.Debugf("Assets requested: %+v", requestedAssets)
+
+	data, err := json.Marshal(requestedAssets)
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to encode to json: %+v", requestedAssets)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewReader(data))
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to create http request, error: %s", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to get http response, error: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return TestAssetsAndroid{}, fmt.Errorf("failed to read response body, error: %s", err)
+		}
+		return TestAssetsAndroid{}, fmt.Errorf("failed to start test: %d, error: %s", resp.StatusCode, string(body))
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to read response body, error: %s", err)
+	}
+
+	err = json.Unmarshal(body, &testAssets)
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to unmarshal response body, error: %s", err)
+	}
+
+	if testAssets.isBundle {
+		testAssets.testApp = &testAssets.Aab
+	} else {
+		testAssets.testApp = &testAssets.Apk
+	}
+	log.Debugf("Uploading file(%s) to (%s)", configs.AppPath, testAssets.testApp.GcsPath)
+
+	err = uploadFile(testAssets.testApp.UploadURL, configs.AppPath)
+	if err != nil {
+		return TestAssetsAndroid{}, fmt.Errorf("failed to upload file(%s) to (%s), error: %s", configs.AppPath, testAssets.testApp.UploadURL, err)
+	}
+
+	if configs.TestType == testTypeInstrumentation {
+		err = uploadFile(testAssets.TestApk.UploadURL, configs.TestApkPath)
+		if err != nil {
+			return TestAssetsAndroid{}, fmt.Errorf("failed to upload file(%s) to (%s), error: %s", configs.TestApkPath, testAssets.TestApk.UploadURL, err)
+		}
+	}
+
+	if len(testAssets.ObbFiles) != len(configs.ObbFiles) {
+		return TestAssetsAndroid{}, fmt.Errorf("invalid length of obb file upload URLs in response: %+v", testAssets)
+	}
+	for i, obbFile := range configs.ObbFiles {
+		if err := uploadFile(testAssets.ObbFiles[i].UploadURL, obbFile); err != nil {
+			return TestAssetsAndroid{}, fmt.Errorf("failed to upload obb file (%s) to (%s), error: %s", obbFile, testAssets.ObbFiles[i].UploadURL, err)
+		}
+	}
+
+	return testAssets, nil
+}
+
+func startTestRun(configs ConfigsModel, testAssets TestAssetsAndroid) error {
+	url := configs.APIBaseURL + "/" + configs.AppSlug + "/" + configs.BuildSlug + "/" + configs.APIToken
+
+	testModel := &testing.TestMatrix{}
+	testModel.EnvironmentMatrix = &testing.EnvironmentMatrix{AndroidDeviceList: &testing.AndroidDeviceList{}}
+
+	testModel.EnvironmentMatrix.AndroidDeviceList.AndroidDevices = configs.TestDevices
+	testModel.FlakyTestAttempts = int64(configs.FlakyTestAttempts)
+
+	// obb files to upload
+	var filesToPush []*testing.DeviceFile
+	for _, obbFile := range testAssets.ObbFiles {
+		filesToPush = append(filesToPush, &testing.DeviceFile{
+			ObbFile: &testing.ObbFile{
+				Obb: &testing.FileReference{
+					GcsPath: obbFile.GcsPath,
+				},
+				ObbFileName: obbFile.Filename,
+			},
+		})
+	}
+
+	// a nil account does not log in to test Google account before test is started
+	var account *testing.Account
+	if configs.AutoGoogleLogin {
+		account = &testing.Account{
+			GoogleAuto: &testing.GoogleAuto{},
+		}
+	}
+
+	testModel.TestSpecification = &testing.TestSpecification{
+		TestTimeout: fmt.Sprintf("%fs", configs.TestTimeout),
+		TestSetup: &testing.TestSetup{
+			EnvironmentVariables: configs.EnvironmentVariables,
+			FilesToPush:          filesToPush,
+			DirectoriesToPull:    configs.DirectoriesToPull,
+			Account:              account,
+		},
+	}
+
+	switch configs.TestType {
+	case testTypeInstrumentation:
+		testModel.TestSpecification.AndroidInstrumentationTest = &testing.AndroidInstrumentationTest{}
+
+		if testAssets.isBundle {
+			testModel.TestSpecification.AndroidInstrumentationTest.AppBundle = &testing.AppBundle{
+				BundleLocation: &testing.FileReference{GcsPath: testAssets.testApp.GcsPath},
+			}
+		} else {
+			testModel.TestSpecification.AndroidInstrumentationTest.AppApk = &testing.FileReference{GcsPath: testAssets.testApp.GcsPath}
+		}
+
+		testModel.TestSpecification.AndroidInstrumentationTest.TestApk = &testing.FileReference{GcsPath: testAssets.TestApk.GcsPath}
+		if configs.AppPackageID != "" {
+			testModel.TestSpecification.AndroidInstrumentationTest.AppPackageId = configs.AppPackageID
+		}
+		if configs.InstTestPackageID != "" {
+			testModel.TestSpecification.AndroidInstrumentationTest.TestPackageId = configs.InstTestPackageID
+		}
+		if configs.InstTestRunnerClass != "" {
+			testModel.TestSpecification.AndroidInstrumentationTest.TestRunnerClass = configs.InstTestRunnerClass
+		}
+		if configs.InstTestTargets != "" {
+			targets := strings.Split(strings.TrimSpace(configs.InstTestTargets), ",")
+			testModel.TestSpecification.AndroidInstrumentationTest.TestTargets = targets
+		}
+		if configs.UseOrchestrator {
+			testModel.TestSpecification.AndroidInstrumentationTest.OrchestratorOption = "USE_ORCHESTRATOR"
+		} else {
+			testModel.TestSpecification.AndroidInstrumentationTest.OrchestratorOption = "DO_NOT_USE_ORCHESTRATOR"
+		}
+		log.Debugf("AndroidInstrumentationTest: %+v", testModel.TestSpecification.AndroidInstrumentationTest)
+	case testTypeRobo:
+		testModel.TestSpecification.AndroidRoboTest = &testing.AndroidRoboTest{}
+
+		if testAssets.isBundle {
+			testModel.TestSpecification.AndroidRoboTest.AppBundle = &testing.AppBundle{
+				BundleLocation: &testing.FileReference{GcsPath: testAssets.testApp.GcsPath},
+			}
+		} else {
+			testModel.TestSpecification.AndroidRoboTest.AppApk = &testing.FileReference{GcsPath: testAssets.testApp.GcsPath}
+		}
+
+		if configs.AppPackageID != "" {
+			testModel.TestSpecification.AndroidRoboTest.AppPackageId = configs.AppPackageID
+		}
+		if configs.RoboInitialActivity != "" {
+			testModel.TestSpecification.AndroidRoboTest.AppInitialActivity = configs.RoboInitialActivity
+		}
+		if configs.RoboMaxDepth != "" {
+			maxDepth, err := strconv.Atoi(configs.RoboMaxDepth)
+			if err != nil {
+				return fmt.Errorf("failed to parse string(%s) to integer, error: %s", configs.RoboMaxDepth, err)
+			}
+			testModel.TestSpecification.AndroidRoboTest.MaxDepth = int64(maxDepth)
+		}
+		if configs.RoboMaxSteps != "" {
+			maxSteps, err := strconv.Atoi(configs.RoboMaxSteps)
+			if err != nil {
+				return fmt.Errorf("failed to parse string(%s) to integer, error: %s", configs.RoboMaxSteps, err)
+			}
+			testModel.TestSpecification.AndroidRoboTest.MaxSteps = int64(maxSteps)
+		}
+		if configs.RoboDirectives != "" {
+			roboDirectives := []*testing.RoboDirective{}
+			scanner := bufio.NewScanner(strings.NewReader(configs.RoboDirectives))
+			for scanner.Scan() {
+				directive := scanner.Text()
+				directive = strings.TrimSpace(directive)
+				if directive == "" {
+					continue
+				}
+
+				directiveParams := strings.Split(directive, ",")
+				if len(directiveParams) != 3 {
+					return fmt.Errorf("invalid directive configuration: %s", directive)
+				}
+				roboDirectives = append(roboDirectives, &testing.RoboDirective{ResourceName: directiveParams[0], InputText: directiveParams[1], ActionType: directiveParams[2]})
+			}
+			testModel.TestSpecification.AndroidRoboTest.RoboDirectives = roboDirectives
+		}
+		if configs.RoboScenarioFile != "" {
+			testModel.TestSpecification.AndroidRoboTest.RoboScript = &testing.FileReference{
+				GcsPath: testAssets.RoboScript.GcsPath,
+			}
+		}
+	case "gameloop":
+		testModel.TestSpecification.AndroidTestLoop = &testing.AndroidTestLoop{}
+
+		if testAssets.isBundle {
+			testModel.TestSpecification.AndroidTestLoop.AppBundle = &testing.AppBundle{
+				BundleLocation: &testing.FileReference{GcsPath: testAssets.testApp.GcsPath},
+			}
+		} else {
+			testModel.TestSpecification.AndroidTestLoop.AppApk = &testing.FileReference{GcsPath: testAssets.testApp.GcsPath}
+		}
+
+		if configs.AppPackageID != "" {
+			testModel.TestSpecification.AndroidTestLoop.AppPackageId = configs.AppPackageID
+		}
+		if configs.LoopScenarios != "" {
+			loopScenarios := []int64{}
+			for _, scenarioStr := range strings.Split(strings.TrimSpace(configs.LoopScenarios), ",") {
+				scenario, err := strconv.Atoi(scenarioStr)
+				if err != nil {
+					return fmt.Errorf("failed to parse string(%s) to integer, error: %s", scenarioStr, err)
+				}
+				loopScenarios = append(loopScenarios, int64(scenario))
+			}
+			testModel.TestSpecification.AndroidTestLoop.Scenarios = loopScenarios
+		}
+		if configs.LoopScenarioLabels != "" {
+			scenarioLabels := strings.Split(strings.TrimSpace(configs.LoopScenarioLabels), ",")
+			testModel.TestSpecification.AndroidTestLoop.ScenarioLabels = scenarioLabels
+		}
+	}
+
+	jsonByte, err := json.Marshal(testModel)
+	if err != nil {
+		return fmt.Errorf("failed to marshal test model, error: %s", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonByte))
+	if err != nil {
+		return fmt.Errorf("failed to create http request, error: %s", err)
+	}
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to get http response, error: %s", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return fmt.Errorf("failed to read response body, error: %s", err)
+		}
+		return fmt.Errorf("failed to start test: %d, error: %s", resp.StatusCode, string(body))
+	}
+
+	return nil
+}
